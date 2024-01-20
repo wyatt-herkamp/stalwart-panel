a4b87127199aba896b32e99bed894c48b832e11a
Wyatt Herkamp <wherkamp@gmail.com>
2024-01-20 10:17:54 -0500

    Update Depends. Also defineModel was stablized.

--- panel-frontend/vite.config.ts
+++ panel-frontend/vite.config.ts
@@ -5,14 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        defineModel: true
-      }
-    }),
-
-  ],
+  plugins: [vue()],
   css: {
     preprocessorOptions: {
       scss: {}
