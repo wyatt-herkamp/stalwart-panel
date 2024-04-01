087a5d6037146440f2e0fea9765140dac772783a
Wyatt Herkamp <wherkamp@gmail.com>
2024-04-01 11:19:29 -0400

    Frontend Updates

--- panel-frontend/src/components/accounts/AccountsListGrid.vue
+++ panel-frontend/src/components/accounts/AccountsListGrid.vue
@@ -1,67 +1,39 @@
 <template>
   <div id="accountsBox" v-if="accounts">
     <div id="accountsSearchAndCreate">
-      <input
-        type="text"
-        id="nameSearch"
-        v-model="searchValue"
-        autofocus
-        placeholder="Search by Name, Username, or Primary Email Address"
-      />
-      <button
-        id="createAccountButton"
-        @click="
-          router.push({
-            name: 'add-account'
-          })
-        "
-      >
+      <input type="text" id="nameSearch" v-model="searchValue" autofocus
+        placeholder="Search by Name, Username, or Primary Email Address" />
+      <button id="createAccountButton" @click="
+    router.push({
+      name: 'create-account'
+    })
+    ">
         Create Account
       </button>
     </div>
     <div id="accounts" class="betterScroll">
       <div class="row" id="header">
-        <div
-          :class="['col', { sorted: sortBy === 'id' }]"
-          @click="sortBy = 'id'"
-          title="Sort by ID"
-        >
+        <div :class="['col', { sorted: sortBy === 'id' }]" @click="sortBy = 'id'" title="Sort by ID">
           ID #
         </div>
-        <div
-          :class="['col', { sorted: sortBy === 'name' }]"
-          @click="sortBy = 'name'"
-          title="Sort by Name"
-        >
+        <div :class="['col', { sorted: sortBy === 'name' }]" @click="sortBy = 'name'" title="Sort by Name">
           Name
         </div>
-        <div
-          :class="['col', { sorted: sortBy === 'username' }]"
-          @click="sortBy = 'username'"
-          title="Sort by Username"
-        >
+        <div :class="['col', { sorted: sortBy === 'username' }]" @click="sortBy = 'username'" title="Sort by Username">
           Username
         </div>
-        <div
-          :class="['col', { sorted: sortBy === 'primary_email' }]"
-          @click="sortBy = 'primary_email'"
-          title="Sort by Primary Email"
-        >
+        <div :class="['col', { sorted: sortBy === 'primary_email' }]" @click="sortBy = 'primary_email'"
+          title="Sort by Primary Email">
           Primary Email
         </div>
         <div class="col description">Description</div>
       </div>
-      <div
-        class="row item"
-        v-for="account in filteredTable"
-        :key="account.id"
-        @click="
-          router.push({
-            name: 'view-account',
-            params: { id: account.id }
-          })
-        "
-      >
+      <div class="row item" v-for="account in filteredTable" :key="account.id" @click="
+    router.push({
+      name: 'view-account',
+      params: { id: account.id }
+    })
+    ">
         <div class="col">{{ account.id }}</div>
         <div class="col" :title="account.name">{{ account.name }}</div>
         <div class="col" :title="account.username">{{ account.username }}</div>
@@ -125,6 +97,7 @@ const filteredTable = computed(() => {
 
 <style scoped lang="scss">
 @import '@/assets/styles/variables';
+
 #accountsSearchAndCreate {
   display: flex;
   flex-direction: row;
@@ -135,13 +108,17 @@ const filteredTable = computed(() => {
   background-color: $color-primary;
   border-radius: 0.5rem;
   box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
+
   h1 {
     margin: 0;
   }
+
   h3 {
     margin: 0;
   }
+
   gap: 1rem;
+
   button {
     margin: 1rem 0 1rem auto;
     padding: 1rem 1rem;
@@ -150,35 +127,42 @@ const filteredTable = computed(() => {
     color: $text-color;
     border: transparent 1px solid;
     width: 25%;
+
     &:hover {
       border: #00bd7e 1px solid;
       transition: all 0.5s ease-in-out;
       color: #00bd7e;
     }
+
     cursor: pointer;
   }
 }
+
 #accountsBox {
   padding: 0 1rem;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
 }
+
 #nameSearch {
   padding: 1rem 1rem;
   margin: 1rem 0;
   box-sizing: border-box;
 }
+
 @media screen and (min-width: 1024px) {
   #accounts {
     max-height: calc(100vh - 6rem);
   }
 }
+
 @media screen and (max-width: 1024px) {
   #accounts {
     max-height: calc(100vh - 8rem);
   }
 }
+
 #accounts {
   background-color: $table-background;
   padding: 1rem;
@@ -193,11 +177,13 @@ const filteredTable = computed(() => {
     cursor: pointer;
     text-align: left;
   }
+
   .col {
     padding: 0.5rem 0.5rem;
     // Hide overflow
     white-space: nowrap;
   }
+
   #header {
     font-weight: bold;
     background-color: $table-header;
@@ -205,11 +191,13 @@ const filteredTable = computed(() => {
     .description {
       cursor: default;
     }
+
     .col {
       border-radius: 0.25rem;
 
       &:not(.description) {
         border: transparent 1px solid;
+
         &:hover {
           border: #00bd7e 1px solid;
           transition: all 0.5s ease-in-out;
@@ -218,64 +206,78 @@ const filteredTable = computed(() => {
       }
     }
   }
+
   .item {
     .col {
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
     }
+
     cursor: pointer;
     border: transparent 1px solid;
     border-radius: 0.25rem;
+
     &:hover {
       border: #00bd7e 1px solid;
       transition: border 0.5s ease-in-out;
     }
+
     &:nth-child(odd) {
       background-color: $table-odd-row;
     }
+
     &:nth-child(even) {
       background-color: $table-even-row;
     }
   }
 }
+
 .sorted {
   border: #00bd7e 1px solid !important;
 }
+
 @media screen and (min-width: 1000px) {
   .row {
     display: grid;
     grid-template-columns: 0.1fr 0.5fr 0.5fr 0.5fr 1fr;
   }
 }
+
 @media screen and (max-width: 600px) {
   .row {
     display: grid;
     grid-template-columns: 0.5fr 1fr 1fr;
     grid-gap: 1rem;
   }
+
   .col {
     &:nth-child(4) {
       display: none;
     }
+
     &:last-child {
       display: none;
     }
   }
+
   #accountsSearchAndCreate {
     flex-direction: column;
     gap: 0;
+
     button {
       margin: 1rem auto;
       width: 100%;
     }
   }
 }
+
 @media screen and (max-width: 1000px) and (min-width: 600px) {
   .row {
     display: grid;
     grid-template-columns: 0.1fr 1fr 1fr 1fr;
   }
+
   .col {
     &:last-child {
       display: none;
